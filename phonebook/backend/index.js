06c42150fb45e6eff6f883b4dfff77a42c18ab80
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2023-05-09 20:44:39 +1000

    express.static added to index.js backend

--- phonebook/backend/index.js
+++ phonebook/backend/index.js
@@ -8,7 +8,7 @@ app.use(morgan(':method :url :status :response-time ms - :res[content-length] :r
 const cors = require('cors')
 
 app.use(cors())
-
+app.use(express.static('build'))
 let persons = [
     { 
       "id": 1,
